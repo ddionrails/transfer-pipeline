6f417852e218fd00d395e5657d24f62a1eaeeb4d
Dominique Hansen <dhansen@diw.de>
2022-08-15 09:39:33 +0000

    Format table_creation.R

--- R/table_creation.R
+++ R/table_creation.R
@@ -1,6 +1,7 @@
 #############################################################################
 # Create aggregated data tables for SOEP transfer project
 #############################################################################
+#' @include cli.R
 
 main <- function() {
   ### Path definition
@@ -14,55 +15,23 @@ main <- function() {
   # Clutters the code, is highly dependent on a static environment.
   # Publishes information about the private path structure and usernames.
   # Instead use command line arguments and/or a config file
-  
-  
+  arguments <- parse_arguments()
+
+
   # Definition of objects
-  dataset <-"p_statistics" # From which data set should values be taken
-  cell_minimum <- 30 # Maximum allowed cell size
+  dataset <- arguments$input # From which data set should values be taken
+  cell_minimum <- arguments$minimal_group_size # Maximum allowed cell size
   year <- "syear" # Survey year must be defined
-  weight_variable <- "phrf" # Weight must be defined
+  weight_variable <- arguments$weight_column # Weight must be defined
   version <- "v37"
-  
-  metadata_path <- paste0("https://git.soep.de/kwenzig/publicecoredoku/raw/master/datasets/",
-                          dataset, "/", version, "/")
+
+  metadata_path <- paste0(
+    "https://git.soep.de/kwenzig/publicecoredoku/raw/master/datasets/",
+    dataset, "/", version, "/"
+  )
   export_path <- "C:/git/soep-transfer/"
   #############################################################################
-  
-  ## load packages
-  ## load data without labels
-  datafile_without_labels <- readstata13::read.dta13(
-    paste0(dataset_path,
-           dataset, ".dta"),
-    convert.factors = FALSE,
-    encoding = "UTF-8"
-  )
-  
-  # Delete cases with no weighting
-  datafile_without_labels <- dplyr::filter(datafile_without_labels,
-                                           weight_variable > 0)
-  
-  # Rename weighting variable to weight
-  names(datafile_without_labels)[names(
-    datafile_without_labels) == weight_variable] <- 'weight'
-  
-  ## load data without labels
-  datafile_with_labels <- readstata13::read.dta13(
-    paste0(dataset_path,
-           dataset, ".dta"),
-    convert.factors = TRUE,
-    nonint.factors = TRUE,
-    encoding = "UTF-8"
-  )
-  
-  # Weights with 0 cause problems
-  datafile_with_labels <- dplyr::filter(datafile_with_labels,
-                                        weight_variable > 0)
-  
-  # Rename weighting variable to weight
-  names(datafile_with_labels)[names(
-    datafile_with_labels) == weight_variable] <- 'weight'
-  
-  # read metainformation
+
   metadaten_variables <-
     read.csv(
       paste0(metadata_path, "variables.csv"),
@@ -70,39 +39,91 @@ main <- function() {
       colClasses = "character",
       encoding = "UTF-8"
     )
-  
+
   metadaten_variable_categories <-
     read.csv(
       paste0(metadata_path, "variable_categories.csv"),
       header = TRUE,
       encoding = "UTF-8"
     )
-  
+
+  ## load packages
+  ## load data without labels
+  datafile_without_labels <- readstata13::read.dta13(
+    paste0(
+      dataset_path,
+      dataset, ".dta"
+    ),
+    convert.factors = FALSE,
+    encoding = "UTF-8"
+  )
+
+  # Delete cases with no weighting
+  datafile_without_labels <- dplyr::filter(
+    datafile_without_labels,
+    weight_variable > 0
+  )
+
+  # Rename weighting variable to weight
+  names(datafile_without_labels)[names(
+    datafile_without_labels
+  ) == weight_variable] <- "weight"
+
+  ## load data without labels
+  datafile_with_labels <- readstata13::read.dta13(
+    paste0(
+      dataset_path,
+      dataset, ".dta"
+    ),
+    convert.factors = TRUE,
+    nonint.factors = TRUE,
+    encoding = "UTF-8"
+  )
+
   # Weights with 0 cause problems
-  metadaten_variable_categories <- dplyr::filter(metadaten_variable_categories,
-                                                 value >= 0)
-  
+  datafile_with_labels <- dplyr::filter(
+    datafile_with_labels,
+    weight_variable > 0
+  )
+
+  # Rename weighting variable to weight
+  names(datafile_with_labels)[names(
+    datafile_with_labels
+  ) == weight_variable] <- "weight"
+
+
+  # Weights with 0 cause problems
+  metadaten_variable_categories <- dplyr::filter(
+    metadaten_variable_categories,
+    value >= 0
+  )
+
   ##############################################################################
   ##############################################################################
   ### Code to create the aggregated tables in variables.csv
   metadaten_variables <-
-    metadaten_variables[metadaten_variables$dataset == dataset,]
-  
-  metadaten_variables_demo <- dplyr::filter(metadaten_variables,
-                                            meantable == "demo")
-  
+    metadaten_variables[metadaten_variables$dataset == dataset, ]
+
+  metadaten_variables_demo <- dplyr::filter(
+    metadaten_variables,
+    meantable == "demo"
+  )
+
   metadaten_variables_demo <- subset(datafile_without_labels,
-                                     select = metadaten_variables_demo$variable)
-  
+    select = metadaten_variables_demo$variable
+  )
+
   # Generate a list that represents all the grouping possibilities of the users
   grouping_variables_list <- get_grouping_variables_list(
-    metadaten_variables_demo = metadaten_variables_demo)
-  
+    metadaten_variables_demo = metadaten_variables_demo
+  )
+
   # Generate a list that represents ne number of differentiations for each
   # possibility
   grouping_count_list <- get_grouping_count_list(
-    metadaten_variables_demo = metadaten_variables_demo)
-  
+    metadaten_variables_demo = metadaten_variables_demo
+  )
+
   ##############################################################################
   # Create aggregated data tables
   # TODO: Loop Body is way to long.
@@ -110,66 +131,74 @@ main <- function() {
   # TODO: Should be split into seperate functions.
   for (var in 1:length(metadaten_variables$variable)) {
     if (metadaten_variables$meantable[var] == "Yes" |
-        metadaten_variables$probtable[var] == "Yes") {
+      metadaten_variables$probtable[var] == "Yes") {
       variable <- metadaten_variables$variable[var]
-      
+
       for (i in seq_along(grouping_variables_list)) {
         grouping_count <- grouping_count_list[[i]]
         grouping_variables <- grouping_variables_list[[i]]
-        
+
         if (!is.na(grouping_variables[1])) {
           grouping_variable_one <- grouping_variables[1]
         } else {
           grouping_variable_one <- ""
         }
-        
+
         if (!is.na(grouping_variables[2])) {
           grouping_variable_two <- grouping_variables[2]
         } else {
           grouping_variable_two <- ""
         }
-        
+
         if (!is.na(grouping_variables[3])) {
           grouping_variable_three <- grouping_variables[3]
         } else {
           grouping_variable_three <- ""
         }
-        
+
         if (metadaten_variables$meantable[var] == "Yes") {
           data <- get_data(
             variable = variable,
             grouping_variables = grouping_variables,
             value_label = FALSE
           )
-          
-          
-          table_numeric <- get_mean_values(dataset = data,
-                                           grouping_variables = 
-                                             grouping_variables)
-          
+
+
+          table_numeric <- get_mean_values(
+            dataset = data,
+            grouping_variables =
+              grouping_variables
+          )
+
           table_numeric <-
-            create_table_lables(table = table_numeric, 
-                                grouping_variables = grouping_variables)
-          
-          protected_table <- get_protected_values(dataset = table_numeric,
-                                                  cell.size = cell_minimum)
-          
-          
+            create_table_lables(
+              table = table_numeric,
+              grouping_variables = grouping_variables
+            )
+
+          protected_table <- get_protected_values(
+            dataset = table_numeric,
+            cell.size = cell_minimum
+          )
+
+
           protected_table <- expand_table(
             table = protected_table,
             table_type = "mean"
           )
-          
+
           data_csv <- get_table_export(
             table = protected_table,
             variable = variable,
             table_type = "mean"
           )
-          
-          json_create(table = protected_table,
-                      variable = variable,
-                      table_type = "mean")
-          
+
+          json_create(
+            table = protected_table,
+            variable = variable,
+            table_type = "mean"
+          )
+
           print(
             paste(
               "The variable",
@@ -180,43 +209,47 @@ main <- function() {
             )
           )
         }
-        
+
         if (datafile_without_labels$probtable[var] == "Yes") {
           data <- get_data(
             variable = variable,
             grouping_variables = grouping_variables,
             value_label = TRUE
           )
-          
+
           if (grouping_variables == "") {
             columns <- c("usedvariable", "year")
           } else {
             columns <- c("usedvariable", "year", grouping_variables)
           }
-          
+
           prop.data <-
-            get_prop_values(dataset = data,
-                            groupvars = columns,
-                            alpha = 0.05)
-          
+            get_prop_values(
+              dataset = data,
+              groupvars = columns,
+              alpha = 0.05
+            )
+
           protected_table <-
             get_protected_values(dataset = prop.data, cell.size = cell_minimum)
-          
+
           protected_table <- expand_table(
             table = protected_table,
             table_type = "prop"
           )
-          
+
           data_csv <- get_table_export(
             table = protected_table,
             variable = variable,
             table_type = "prop"
           )
-          
-          json_create(table = protected_table,
-                      variable = variable,
-                      table_type = "prop")
-          
+
+          json_create(
+            table = protected_table,
+            variable = variable,
+            table_type = "prop"
+          )
+
           print(
             paste(
               "The variable",
