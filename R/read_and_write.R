ed24bc7f32e2e35a8ac03e3647390a9f82550d2c
Dominique Hansen <dhansen@diw.de>
2022-08-22 12:04:18 +0000

    Update read_and_write.R

--- R/read_and_write.R
+++ R/read_and_write.R
@@ -2,8 +2,22 @@
 #' @export get_labels_for_dataset
 get_labels_for_dataset <- function(dataset_name, label_metadata) {
     label_metadata <- label_metadata[label_metadata$dataset == dataset_name]
-    return(list(
-        "age" = list("1" = "17-34", "2" = "35-65"),
-        "no_age" = list("1" = "")
-    ))
+    variables <- as.vector(label_metadata$variable)
+    labels <- as.vector(label_metadata$label)
+    values <- as.vector(label_metadata$value)
+    value_labels <- list()
+    for (row_index in seq_along(variables)) {
+        value_label <- list()
+
+        value_label[[values[row_index]]] <- labels[row_index]
+
+        if (variables[row_index] %in% value_labels) {
+
+        }
+        value_labels[[variables[row_index]]] <- append(
+            value_labels[[variables[row_index]]],
+            value_label
+        )
+    }
+    return(value_labels)
 }
